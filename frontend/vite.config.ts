ccb557c0476d1851afeca30084744981372fd9c1
Gilbert Brandow <simon@0100.se>
2026-05-29 17:44:59 +0200

    Confifured Sentry frontend (#139)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 const backendUrl = process.env['VITE_BACKEND_URL'] ?? 'http://localhost:8000'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'woodpecker',
+      project: 'woodpecker-frontend',
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
   server: {
     proxy: {
       '/api': {
